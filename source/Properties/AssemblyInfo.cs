e0e0335ee0732bdf0eb9786d258709fcb8600a0e
Andy Borka <ajborka@users.noreply.github.com>
2022-01-10 16:29:22 -0500

    Version bump to 22.1

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Talking Flight Monitor Development Group")]
 [assembly: AssemblyProduct("Talking Flight Monitor")]
-[assembly: AssemblyCopyright("Copyright ©  2018-2021")]
+[assembly: AssemblyCopyright("Copyright ©  2018-2022 Jason Fayre and Andy Borka")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.18.4")]
-[assembly: AssemblyFileVersion("2.0.18.4")]
+[assembly: AssemblyVersion("22.1.0.0")]
+[assembly: AssemblyFileVersion("22.1.0.0")]
