bd10bf0314f86928920287302325f893f5cd2796
Andy Borka <ajborka@users.noreply.github.com>
2022-10-23 08:36:48 -0400

    PMDG 737: Partial forward panel
    Added around 50% of the forward panel. Will revisit after adding move to... features that a higher priority at this time.

---
+++ source/PMDG/PMDG 737/CockpitPanels/Forward/ctlForwardBrakes.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace tfm.PMDG.PMDG_737.CockpitPanels.Forward
+{
+    partial class ctlForwardBrakes
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.brakesFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.autoBrakeLabel = new System.Windows.Forms.Label();
+            this.autoBrakeTextBox = new System.Windows.Forms.TextBox();
+            this.brakesFlowLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // brakesFlowLayoutPanel
+            // 
+            this.brakesFlowLayoutPanel.AutoSize = true;
+            this.brakesFlowLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.brakesFlowLayoutPanel.Controls.Add(this.autoBrakeLabel);
+            this.brakesFlowLayoutPanel.Controls.Add(this.autoBrakeTextBox);
+            this.brakesFlowLayoutPanel.Location = new System.Drawing.Point(0, -123);
+            this.brakesFlowLayoutPanel.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.brakesFlowLayoutPanel.Name = "brakesFlowLayoutPanel";
+            this.brakesFlowLayoutPanel.Size = new System.Drawing.Size(0, 0);
+            this.brakesFlowLayoutPanel.TabIndex = 0;
+            // 
+            // autoBrakeLabel
+            // 
+            this.autoBrakeLabel.AutoSize = true;
+            this.autoBrakeLabel.Location = new System.Drawing.Point(10, 0);
+            this.autoBrakeLabel.Margin = new System.Windows.Forms.Padding(10, 0, 3, 0);
+            this.autoBrakeLabel.Name = "autoBrakeLabel";
+            this.autoBrakeLabel.Size = new System.Drawing.Size(138, 33);
+            this.autoBrakeLabel.TabIndex = 13;
+            this.autoBrakeLabel.Text = "Au&to brake";
+            // 
+            // autoBrakeTextBox
+            // 
+            this.autoBrakeTextBox.AccessibleName = "Auto brake";
+            this.autoBrakeTextBox.Location = new System.Drawing.Point(10, 36);
+            this.autoBrakeTextBox.Margin = new System.Windows.Forms.Padding(10, 3, 3, 3);
+            this.autoBrakeTextBox.Name = "autoBrakeTextBox";
+            this.autoBrakeTextBox.ReadOnly = true;
+            this.autoBrakeTextBox.Size = new System.Drawing.Size(60, 40);
+            this.autoBrakeTextBox.TabIndex = 14;
+            this.autoBrakeTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.autoBrakeTextBox.Enter += new System.EventHandler(this.autoBrakeTextBox_Enter);
+            this.autoBrakeTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.autoBrakeTextBox_KeyDown);
+            // 
+            // ctlForwardBrakes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Controls.Add(this.brakesFlowLayoutPanel);
+            this.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.Name = "ctlForwardBrakes";
+            this.Size = new System.Drawing.Size(227, 0);
+            this.brakesFlowLayoutPanel.ResumeLayout(false);
+            this.brakesFlowLayoutPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel brakesFlowLayoutPanel;
+        private System.Windows.Forms.Label autoBrakeLabel;
+        private System.Windows.Forms.TextBox autoBrakeTextBox;
+    }
+}
---
+++ source/PMDG/PMDG 737/CockpitPanels/Forward/ctlForwardBrakes.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tfm.PMDG.PMDG_737.CockpitPanels.Forward
+{
+    public partial class ctlForwardBrakes : UserControl
+    {
+        public ctlForwardBrakes()
+        {
+            InitializeComponent();
+        }
+
+        private void autoBrakeTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Alt && e.KeyCode == Keys.D1) ||
+    (e.Alt && e.KeyCode == Keys.D2) ||
+    (e.Alt && e.KeyCode == Keys.D3)) return;
+            if (e.KeyCode == Keys.O)
+            {
+                PMDG737Aircraft.AutoBrake(1);
+            }
+            if (e.KeyCode == Keys.R)
+            {
+                PMDG737Aircraft.AutoBrake(0);
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                PMDG737Aircraft.AutoBrake(2);
+            }
+            if (e.KeyCode == Keys.D1)
+            {
+                PMDG737Aircraft.AutoBrake(3);
+            }
+            if (e.KeyCode == Keys.D2)
+            {
+                PMDG737Aircraft.AutoBrake(4);
+            }
+            if (e.KeyCode == Keys.D3)
+            {
+                PMDG737Aircraft.AutoBrake(5);
+            }
+
+        }
+
+        private void autoBrakeTextBox_Enter(object sender, EventArgs e)
+        {
+            autoBrakeTextBox.Select(autoBrakeTextBox.Text.Length, 0);
+        }
+    }
+}
