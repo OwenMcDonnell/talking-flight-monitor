55a49b4f1c23d317708b218b2f5627b23b3bbf2c
Jason Fayre <jfayre@gmail.com>
2022-10-30 21:40:38 -0400

    hotkey changes
    Added Right Bracket, Question Mark to bring up keyboard manager.
    Changed settings key to Right Bracket, Ctrl+Comma.
    Added button in keyboard manager to execute the selected command in the list.

--- source/Properties/Hotkeys.Designer.cs
+++ source/Properties/Hotkeys.Designer.cs
@@ -12,7 +12,7 @@ namespace tfm.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
     internal sealed partial class Hotkeys : global::System.Configuration.ApplicationSettingsBase {
         
         private static Hotkeys defaultInstance = ((Hotkeys)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Hotkeys())));
@@ -1078,7 +1078,7 @@ namespace tfm.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Shift+OemQuestion")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Ctrl+Oemcomma")]
         public global::System.Windows.Forms.Keys application_settings {
             get {
                 return ((global::System.Windows.Forms.Keys)(this["application_settings"]));
@@ -1205,6 +1205,18 @@ namespace tfm.Properties {
             }
             set {
                 this["JumpToRunway"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Shift+OemQuestion")]
+        public global::System.Windows.Forms.Keys keyboard_manager {
+            get {
+                return ((global::System.Windows.Forms.Keys)(this["keyboard_manager"]));
+            }
+            set {
+                this["keyboard_manager"] = value;
             }
         }
     }
