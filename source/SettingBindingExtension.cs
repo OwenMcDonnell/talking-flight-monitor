f697734d0cc44b5aabd57ca90bfaf2670339e2ef
Jason Fayre <jfayre@gmail.com>
2023-04-27 21:15:35 -0400

    added SettingsBindingExtention class to bind settings in xaml

---
+++ source/SettingBindingExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Data;
+
+namespace tfm
+{
+    public class SettingBindingExtension : System.Windows.Data.Binding
+    {
+        public SettingBindingExtension()
+        {
+            Initialize();
+        }
+
+        public SettingBindingExtension(string path)
+            : base(path)
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            this.Source = tfm.Properties.Settings.Default;
+            this.Mode = BindingMode.TwoWay;
+        }
+    }
+}
