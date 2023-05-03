b2e1532212b7d5793c3e94d2212bb9aacda4a7c3
Andy Borka <ajborka@users.noreply.github.com>
2023-05-02 20:13:22 -0400

    PMDG 737 FMC pages
    The PMDG 737 FMC pages have button click events and key bindings assigned to them. All the key commands work so far. Sometimes NVDA will not read the current control name/state. The work around is to tab around to a few other controls to clear NVDA's buffer. Also added a new control that sits just to the right of the scratchpad. It is a single letter indicator (A or D) that indicates the current line select mode. A = alternate/F1-F12, and D = default (ctrl/alt 1-6). Access the indicator with ALT+A. The next new key command is CTRL+L, which is a convenience command that cycles between alternate and default line select modes. It will be functional in a later build.

--- source/PMDG/PMDG 737/Forms/CduDialog.xaml.cs
+++ source/PMDG/PMDG 737/Forms/CduDialog.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FSUIPC;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -26,6 +27,7 @@ namespace tfm.PMDG.PMDG_737.Forms
             InitializeComponent();
 
             RefreshCDU();
+            cduDisplay.Focus();
         }
 
         private void RefreshCDU()
@@ -47,9 +49,220 @@ namespace tfm.PMDG.PMDG_737.Forms
                 this.Title = "CDU not powered";
                 cduDisplay.Text = "The CDU is powered off.";
             }
+                                }
 
+        //Key command executed events.
+        #region "Keyboard commands"
+
+        private void FocusCDUDisplay(object sender, ExecutedRoutedEventArgs e)
+        {
+            cduDisplay.Focus();
+        }
+
+        private void FocusScratchpadExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            ScratchpadTextBox.Focus();
+        }
+
+        private void FocusLineSelectMode(object sender, ExecutedRoutedEventArgs e)
+        {
+            lineSelectModeTextBox.Focus();
+        }
+
+        private void ActivateInitRefPage(object sender, ExecutedRoutedEventArgs e)
+        {
+            initRefButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+        private void ActivateRtePage(object sender, ExecutedRoutedEventArgs e)
+        {
+            rteButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+private void ActivateClbPage(object sender, ExecutedRoutedEventArgs e)
+        {
+            clbButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+private void ActivateCrzPage(object sender, ExecutedRoutedEventArgs e)
+        {
+            crzButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+        private void ActivateLegsPage(object sender, ExecutedRoutedEventArgs e)
+        {
+            legsButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+        private void ActivateDepArPage(object sender, ExecutedRoutedEventArgs e)
+        {
+            depArButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+        private void ActivateHoldPage(object sender, ExecutedRoutedEventArgs e)
+        {
+            holdButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+        private void ActivateProgPage(object sender, ExecutedRoutedEventArgs e)
+        {
+            progButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+        private void ActivateN1Page(object sender, ExecutedRoutedEventArgs e)
+        {
+            n1LimitButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+        private void ActivateFixPage(object sender, ExecutedRoutedEventArgs e)
+        {
+            fixButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+private void ActivatePreviousPage(object sender, ExecutedRoutedEventArgs e)
+        {
+            prevButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+        private void ActivateNextPage(object sender, ExecutedRoutedEventArgs e)
+        {
+            nextButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+        private void ActivateMenuPage(object sender, ExecutedRoutedEventArgs e)
+        {
+            menuButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+        private void ExecuteCDUAction(object sender, ExecutedRoutedEventArgs e)
+        {
+            execButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+        private void ClearScratchpad(object sender, ExecutedRoutedEventArgs e)
+        {
+            clearButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+        private void RefreshCDUAction(object sender, ExecutedRoutedEventArgs e)
+        {
+            refreshButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+        private void DeleteAction(object sender, ExecutedRoutedEventArgs e)
+        {
+            deleteButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Button.ClickEvent));
+        }
+
+        private void ChangeLineSelectKeys(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+        #endregion
+        
+        // Button click events.
+        #region "button click events"
+        private void initRefButton_Click(object sender, RoutedEventArgs e)
+        {
+            FSUIPCConnection.SendControlToFS(PMDG_737_NGX_Control.EVT_CDU_L_INIT_REF, Aircraft.ClkL);
+            RefreshCDU();
+        }
+
+                                        private void rteButton_Click(object sender, RoutedEventArgs e)
+        {
+            FSUIPCConnection.SendControlToFS(PMDG_737_NGX_Control.EVT_CDU_L_RTE, Aircraft.ClkL);
+            RefreshCDU();
 
         }
 
+        private void clbButton_Click(object sender, RoutedEventArgs e)
+        {
+            FSUIPCConnection.SendControlToFS(PMDG_737_NGX_Control.EVT_CDU_L_CLB, Aircraft.ClkL);
+            RefreshCDU();
+        }
+
+        private void crzButton_Click(object sender, RoutedEventArgs e)
+        {
+            FSUIPCConnection.SendControlToFS(PMDG_737_NGX_Control.EVT_CDU_L_CRZ, Aircraft.ClkL);
+            RefreshCDU();
+        }
+
+        private void legsButton_Click(object sender, RoutedEventArgs e)
+        {
+            FSUIPCConnection.SendControlToFS(PMDG_737_NGX_Control.EVT_CDU_L_LEGS, Aircraft.ClkL);
+            RefreshCDU();
+        }
+
+        private void depArButton_Click(object sender, RoutedEventArgs e)
+        {
+            FSUIPCConnection.SendControlToFS(PMDG_737_NGX_Control.EVT_CDU_L_DEP_ARR, Aircraft.ClkL);
+            RefreshCDU();
+        }
+
+        private void holdButton_Click(object sender, RoutedEventArgs e)
+        {
+            FSUIPCConnection.SendControlToFS(PMDG_737_NGX_Control.EVT_CDU_L_HOLD, Aircraft.ClkL);
+            RefreshCDU();
+        }
+
+        private void progButton_Click(object sender, RoutedEventArgs e)
+        {
+            FSUIPCConnection.SendControlToFS(PMDG_737_NGX_Control.EVT_CDU_L_PROG, Aircraft.ClkL);
+            RefreshCDU();
+        }
+
+        private void n1LimitButton_Click(object sender, RoutedEventArgs e)
+        {
+            FSUIPCConnection.SendControlToFS(PMDG_737_NGX_Control.EVT_CDU_L_N1_LIMIT, Aircraft.ClkL);
+            RefreshCDU();
+        }
+
+        private void fixButton_Click(object sender, RoutedEventArgs e)
+        {
+            FSUIPCConnection.SendControlToFS(PMDG_737_NGX_Control.EVT_CDU_L_FIX, Aircraft.ClkL);
+            RefreshCDU();
+        }
+
+        private void prevButton_Click(object sender, RoutedEventArgs e)
+        {
+            FSUIPCConnection.SendControlToFS(PMDG_737_NGX_Control.EVT_CDU_L_PREV_PAGE, Aircraft.ClkL);
+            RefreshCDU();
+        }
+
+        private void nextButton_Click(object sender, RoutedEventArgs e)
+        {
+            FSUIPCConnection.SendControlToFS(PMDG_737_NGX_Control.EVT_CDU_L_NEXT_PAGE, Aircraft.ClkL);
+            RefreshCDU();
+        }
+
+        private void menuButton_Click(object sender, RoutedEventArgs e)
+        {
+            FSUIPCConnection.SendControlToFS(PMDG_737_NGX_Control.EVT_CDU_L_MENU, Aircraft.ClkL);
+            RefreshCDU();
+        }
+
+        private void execButton_Click(object sender, RoutedEventArgs e)
+        {
+            FSUIPCConnection.SendControlToFS(PMDG_737_NGX_Control.EVT_CDU_L_EXEC, Aircraft.ClkL);
+            RefreshCDU();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            FSUIPCConnection.SendControlToFS(PMDG_737_NGX_Control.EVT_CDU_L_CLR, Aircraft.ClkL);
+            RefreshCDU();
+        }
+
+        private void refreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshCDU();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            FSUIPCConnection.SendControlToFS(PMDG_737_NGX_Control.EVT_CDU_L_DEL, Aircraft.ClkL);
+            RefreshCDU();
+        }
+        #endregion
     }
 }
