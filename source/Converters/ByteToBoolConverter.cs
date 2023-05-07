a07ba8f9fc21b8d39d7025f4f2ec70fb89ac3446
Andy Borka <ajborka@users.noreply.github.com>
2023-05-07 09:52:49 -0400

    Converters: ByteToBoolConverter
    Added a byte to bool converter that makes it easier to use the Toggle.CurrentState property. When assigning a byte value of 0 or 1 to the on/off state of a toggle button in WPF, the byte to bool converter automatically changes the 0 or 1 to true/false.

---
+++ source/Converters/ByteToBoolConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace tfm.Converters
+{
+
+    public class ByteToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is byte byteValue)
+            {
+                return byteValue == 1;
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? (byte)1 : (byte)0;
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+    }
+
+}
