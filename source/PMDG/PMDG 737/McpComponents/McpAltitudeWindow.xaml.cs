2251a3866cee391395f1e223d2ca14145c3d01de
Andy Borka <ajborka@users.noreply.github.com>
2023-05-08 15:51:39 -0400

    PMDG737: Altitude window
    Have most of the layout for the altitude window done. The toggle buttons use a byte to bool converter to change SingleStateToggle.CurrentState.Key to a true/false value. This should make things easier to show in control state (a toggle button pressed/not pressed). It is also easy to reverse the true/false value if needed. Works good with panel controls or indicators where the light is on to indicate something is turned off. An example of how to use it is in the BuildToggleButton method of the altitude window.

--- source/PMDG/PMDG 737/McpComponents/McpAltitudeWindow.xaml.cs
+++ source/PMDG/PMDG 737/McpComponents/McpAltitudeWindow.xaml.cs
@@ -1,27 +1,51 @@
-﻿using System;
+﻿using tfm.Converters;
+using tfm.PMDG.PanelObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Automation;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Globalization;
 
 namespace tfm.PMDG.PMDG_737.McpComponents
 {
-    /// <summary>
-    /// Interaction logic for McpAltitudeWindow.xaml
-    /// </summary>
-    public partial class McpAltitudeWindow : Window
+        public partial class McpAltitudeWindow : Window
     {
+
+       ByteToBoolConverter converter = new ByteToBoolConverter();
         public McpAltitudeWindow()
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var vNavToggle = PMDG737Aircraft.PanelControls.Where(x => x.Offset == Aircraft.pmdg737.MCP_annunVNAV).ToArray()[0] as SingleStateToggle;
+            var lvlChangeToggle = PMDG737Aircraft.PanelControls.Where(x => x.Offset == Aircraft.pmdg737.MCP_annunLVL_CHG).ToArray()[0] as SingleStateToggle;
+            var holdToggle = PMDG737Aircraft.PanelControls.Where(x => x.Offset == Aircraft.pmdg737.MCP_annunALT_HOLD).ToArray()[0] as SingleStateToggle;
+
+            BuildToggleButton(vNavButton, vNavToggle, "Vnav");
+            BuildToggleButton(lvlChangeButton, lvlChangeToggle, "Level change");
+            BuildToggleButton(holdButton, holdToggle, "Hold");
+                    }
+
+        private void BuildToggleButton(ToggleButton control, SingleStateToggle toggle, string alternateName = null, bool reverse = false)
+        {
+            string name = alternateName == null ? toggle.Name : alternateName;
+
+            control.Content = $"{name} {toggle.CurrentState.Value}";
+            control.IsChecked = reverse? !(bool?)converter.Convert(toggle.CurrentState.Key, typeof(bool?), null, CultureInfo.InvariantCulture) : (bool?)converter.Convert(toggle.CurrentState.Key, typeof(bool?), null, CultureInfo.InvariantCulture);
+            AutomationProperties.SetName(control, $"{name} {toggle.CurrentState.Value}");
+                                }
     }
 }
