22663168a668f650ca7d5285e8c0c8a4695bc185
Andy Borka <ajborka@users.noreply.github.com>
2023-05-16 18:29:40 -0400

    PMDG 737 MCP altitude box
    Made the contents of the altitude input field selected when it has keyboard focus.

--- source/PMDG/PMDG 737/McpComponents/McpAltitudeWindow.xaml.cs
+++ source/PMDG/PMDG 737/McpComponents/McpAltitudeWindow.xaml.cs
@@ -109,5 +109,10 @@ namespace tfm.PMDG.PMDG_737.McpComponents
         {
             this.Hide();
         }
+
+        private void altitudeTextBox_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            altitudeTextBox.SelectAll();
+        }
     }
 }
