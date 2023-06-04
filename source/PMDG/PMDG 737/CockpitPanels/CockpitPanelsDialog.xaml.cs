b18e1b1162a9b348eafa4afb72cb551598e66719
Andy Borka <ajborka@users.noreply.github.com>
2023-06-04 15:28:44 -0400

    PMDG 737 overhead gear
    Added the Overhead/gear panel. It is fully functional except key commands.

--- source/PMDG/PMDG 737/CockpitPanels/CockpitPanelsDialog.xaml.cs
+++ source/PMDG/PMDG 737/CockpitPanels/CockpitPanelsDialog.xaml.cs
@@ -55,6 +55,7 @@ namespace tfm.PMDG.PMDG_737.CockpitPanels
             panelMappings["domeLights"] = new UserControlInfo { control = new OverheadDomeLights(), Keywords = new[] { "Overhead", "dome", "lights", "interior", "Cockpit" } };
             panelMappings["eec"] = new UserControlInfo { control = new OverheadEEC(), Keywords = new[] { "electrical", "engines", "power", "navigation", "reversers" } };
             panelMappings["cvr"] = new UserControlInfo { control = new OverheadCvr(), Keywords = new[] { "cvr", "CVR", "voice recorder", "flight recorder", "data recorder", "test" } };
+            panelMappings["gear"] = new UserControlInfo { control = new OverheadGear(), Keywords = new[] { "Overhead", "lights", "Landing gear", "gear" } };
         }
               
                 private void panelsTreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
---
+++ source/PMDG/PMDG 737/CockpitPanels/OverheadGear.xaml.cs
@@ -0,0 +1,60 @@
+﻿using tfm.PMDG.PanelObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using UserControl = System.Windows.Controls.UserControl;
+using System.Windows.Threading;
+
+namespace tfm.PMDG.PMDG_737.CockpitPanels
+{
+        public partial class OverheadGear : UserControl
+    {
+
+        private SingleStateToggle noseGearLight = PMDG737Aircraft.PanelControls.Where(x => x.Offset == Aircraft.pmdg737.GEAR_annunOvhdNOSE).First() as SingleStateToggle;
+        private SingleStateToggle leftGearLight = PMDG737Aircraft.PanelControls.Where(x => x.Offset == Aircraft.pmdg737.GEAR_annunOvhdLEFT).First() as SingleStateToggle;
+        private SingleStateToggle rightGearLight = PMDG737Aircraft.PanelControls.Where(x => x.Offset == Aircraft.pmdg737.GEAR_annunOvhdRIGHT).First() as SingleStateToggle;
+        
+        public OverheadGear()
+        {
+            InitializeComponent();
+        }
+
+        private async void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            var timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(300)
+            };
+            timer.Tick += async (s, args) => await UpdatePanelControlsAsync();
+            timer.Start();
+
+        }
+
+        private async Task UpdatePanelControlsAsync()
+        {
+            await Task.Run(() =>
+            {
+
+                Dispatcher.Invoke(() =>
+                {
+                    App.UI.BuildIndicatorTextBox(noseGearTextBox, noseGearLight, "Nose gear indicator");
+                    App.UI.BuildIndicatorTextBox(leftGearTextBox, leftGearLight, "Left gear indicator");
+                    App.UI.BuildIndicatorTextBox(rightGearTextBox, rightGearLight, "Right gear indicator");
+                });
+            });
+        }
+
+    }
+}
